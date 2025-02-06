6b2d616bc97f36d12dcb7c13bba9bdd713d3cd2a
Benson <nyota.benson752@gmail.com>
2025-02-06 19:18:26 +0300

    updates

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import os
 
 GTM_ID = "GTM-P7W3S69H"  # Replace with your GTM ID
 
-GTM_SCRIPT = f"""
+GTM_SCRIPT = r"""
 <script>(function(w,d,s,l,i){{w[l]=w[l]||[];w[l].push({{'gtm.start':
 new Date().getTime(),event:'gtm.js'}});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
